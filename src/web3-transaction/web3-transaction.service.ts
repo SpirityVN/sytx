e1d44a8b80d7d9725c12015bbe23f847e0aaac73
Hoan Do <hoan.do@icetea.io>
2022-11-23 17:49:28 +0700

    refactor: update todo

--- src/web3-transaction/web3-transaction.service.ts
+++ src/web3-transaction/web3-transaction.service.ts
@@ -1,12 +1,12 @@
-import { PrismaService } from 'nestjs-prisma';
 import { BadRequestException, Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { ethers, Contract } from 'ethers';
+import { contract, event } from '@prisma/client';
 import Axios from 'axios';
-import { BaseService } from 'src/_services/base.service';
-import { GetEventByTxHash, CreateContractInput } from './web3-transaction.dto';
-import { contract, event, Prisma } from '@prisma/client';
+import { Contract, ethers } from 'ethers';
 import { find, isEmpty, map, pick } from 'lodash';
+import { PrismaService } from 'nestjs-prisma';
+import { BaseService } from 'src/_services/base.service';
+import { CreateContractInput, GetEventByTxHash } from './web3-transaction.dto';
 @Injectable()
 export class Web3TransactionService extends BaseService implements OnModuleInit {
   constructor(private readonly configService: ConfigService, private readonly prismaService: PrismaService) {
@@ -15,6 +15,16 @@ export class Web3TransactionService extends BaseService implements OnModuleInit 
   private provider: ethers.providers.JsonRpcProvider;
   onModuleInit() {
     this.provider = new ethers.providers.WebSocketProvider(this.configService.get('RPC_NETWORK_URL') as string);
+  }
+
+  private _transformArgsEvent(values: ethers.utils.Result, keys: any[]) {
+    let args = {};
+
+    if (values.length !== keys.length) return;
+    keys.forEach((key, i) => {
+      args[key] = values[i];
+    });
+    return args;
   }
 
   async getEventByTxHash(
@@ -42,6 +52,8 @@ export class Web3TransactionService extends BaseService implements OnModuleInit 
 
     let eventByTxHashRaws = await contract.queryFilter(eventFilter, transaction.blockNumber);
 
+    //TODO: get transaction over 50000 blocks
+
     if (!eventByTxHashRaws || isEmpty(eventByTxHashRaws)) return;
 
     return Promise.all(
@@ -49,7 +61,7 @@ export class Web3TransactionService extends BaseService implements OnModuleInit 
         ...pick(eventByTxHashRaw, ['event', 'blockNumber', 'blockHash', 'transactionHash']),
         ...pick(await eventByTxHashRaw.getTransactionReceipt(), ['from', 'to', 'status']),
         timestamp: (await eventByTxHashRaw.getBlock()).timestamp,
-        eventValue: this.transformArgsEvent(eventByTxHashRaw.args, eventContractDetail.params as String[]),
+        eventValue: this._transformArgsEvent(eventByTxHashRaw.args, eventContractDetail.params as String[]),
       })),
     );
   }
@@ -80,13 +92,7 @@ export class Web3TransactionService extends BaseService implements OnModuleInit 
     });
   }
 
-  transformArgsEvent(values: ethers.utils.Result, keys: any[]) {
-    let args = {};
-
-    if (values.length !== keys.length) return;
-    keys.forEach((key, i) => {
-      args[key] = values[i];
-    });
-    return args;
+  updateContract() {
+    //TODO: code here
   }
 }
